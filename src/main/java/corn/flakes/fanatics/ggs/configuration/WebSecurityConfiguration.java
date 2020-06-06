2388a338f49f99e30dd4be0c760f71ed328ba43b
vvisnia <ziomek131212@wp.pl>
2020-06-06 15:50:17 +0200

    Added basic configuration for OAuth2 with Google.

---
+++ src/main/java/corn/flakes/fanatics/ggs/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package corn.flakes.fanatics.ggs.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .oauth2Login();
+    }
+    
+}
