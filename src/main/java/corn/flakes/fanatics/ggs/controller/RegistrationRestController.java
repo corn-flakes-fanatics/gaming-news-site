596219b0761b9218937362370b17c38c856ede1d
jsildatk <jakub.sildatk@gmail.com>
2020-06-10 16:01:35 +0200

    Endpoints for checking if user exists by login, username and email

--- src/main/java/corn/flakes/fanatics/ggs/controller/RegistrationRestController.java
+++ src/main/java/corn/flakes/fanatics/ggs/controller/RegistrationRestController.java
@@ -8,11 +8,9 @@ import corn.flakes.fanatics.ggs.service.RegistrationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import static corn.flakes.fanatics.ggs.service.RegistrationService.REGISTER_MAPPING;
+import static corn.flakes.fanatics.ggs.service.RegistrationService.*;
 import static corn.flakes.fanatics.ggs.utils.UriUtils.createFromId;
 
 @RestController
@@ -27,6 +25,24 @@ public class RegistrationRestController {
         final UserModel registeredUser = registrationService.registerUser(registrationDTO);
         return ResponseEntity.created(createFromId(registeredUser.getId()))
                 .body(new ResponseMessage<>(MessageCode.REGISTERED.getMessage(), registeredUser));
+    }
+    
+    @GetMapping(value = CHECK_LOGIN_MAPPING)
+    public ResponseMessage<String> checkLogin(@PathVariable String login) {
+        final boolean loginExists = registrationService.loginExists(login);
+        return new ResponseMessage<>(String.valueOf(loginExists));
+    }
+    
+    @GetMapping(value = CHECK_USERNAME_MAPPING)
+    public ResponseMessage<String> checkUsername(@PathVariable String username) {
+        final boolean usernameExists = registrationService.usernameExists(username);
+        return new ResponseMessage<>(String.valueOf(usernameExists));
+    }
+    
+    @GetMapping(value = CHECK_EMAIL_MAPPING)
+    public ResponseMessage<String> checkEmail(@PathVariable String email) {
+        final boolean emailExists = registrationService.emailExists(email);
+        return new ResponseMessage<>(String.valueOf(emailExists));
     }
     
 }
